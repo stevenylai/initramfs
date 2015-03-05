ff0221dd08b368c34918d15a4d1eba855ff2c860
Steven Lai <steven.lai@liricco.com>
2015-03-05 10:25:31 +0800

    Use xz for compression

--- install_initramfs.sh
+++ install_initramfs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-INITRAMFS="initramfs-`eselect kernel show|grep linux|sed 's/^[^0-9]*//g'`.cpio.lzma"
+#INITRAMFS="initramfs-`eselect kernel show|grep linux|sed 's/^[^0-9]*//g'`.cpio.lzma"
+INITRAMFS="initramfs-`eselect kernel show|grep linux|sed 's/^[^0-9]*//g'`.cpio.xz"
 if [ -f /boot/${INITRAMFS} ]; then
 	mv /boot/${INITRAMFS} /boot/${INITRAMFS}.old
 fi
 cd /usr/src/initramfs
-find . -print0 | cpio --null -ov --format=newc | lzma -9 > /boot/${INITRAMFS}
+#find . -print0 | cpio --null -ov --format=newc | lzma -9 > /boot/${INITRAMFS}
+find . -print0 | cpio --null -ov --format=newc | xz --check=crc32 --lzma2=dict=512KiB > /boot/${INITRAMFS}
